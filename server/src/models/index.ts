51367fa2cc8c3666dbcf472991a63069731cef2a
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-08-09 14:43:35 -0400

    Removing result model

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,6 @@
 // Export all models and their interfaces
 export { Driver, type IDriver } from './Driver';
 export { Race, type IRace } from './Race';
-export { Result, type IResult } from './Result';
 export { User, type IUser } from './User';
 export { FantasyTeam, type IFantasyTeam } from './FantasyTeam';
 export { Constructor, type IConstructor } from './Constructor';
